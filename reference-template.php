1fa5dab8ff732facd44ba51004b0af9a5734acf0
BE-Webdesign <cromleyedwin89@gmail.com>
2017-01-01 18:51:19 -0500

    Improve themability.
    The plugin now adds a template override when `/rest-reference/` is
    accessed.  This is a first step towards better theme integration.

---
+++ reference-template.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template file for loading the reference into the context of a theme.
+ *
+ * @package WP_REST_API_Reference
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Hook in content wrappers.
+add_action( 'wp_rest_api_reference_before_main_content', 'wp_rest_api_reference_content_wapper_start' );
+add_action( 'wp_rest_api_reference_after_main_content', 'wp_rest_api_reference_content_wapper_end' );
+
+/**
+ * Hook: wp_rest_api_reference_before_main_content.
+ *
+ * @hooked wp_rest_api_reference_content_wapper_start - 10 (outputs opening divs for the content)
+ */
+do_action( 'wp_rest_api_reference_before_main_content' );
+
+?>
+<h1>HI!!!!!</h1>
+<div id="wp-rest-api-reference" class="wp-rest-api-reference"></div>
+<?php
+
+/**
+ * Hook: wp_rest_api_reference_after_main_content.
+ *
+ * @hooked wp_rest_api_reference_content_wapper_end - 10 (outputs opening divs for the content)
+ */
+do_action( 'wp_rest_api_reference_after_main_content' );
