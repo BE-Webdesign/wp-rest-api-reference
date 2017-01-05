d37a3350cbcb9e80320bd2dd6f456c188e5b0155
BE-Webdesign <cromleyedwin89@gmail.com>
2017-01-04 22:54:52 -0500

    Fix ResourcesMenu React errors.
    The ResourcesMenu is missing the key value for each resource.

--- src/ui/components/ResourcesMenu.js
+++ src/ui/components/ResourcesMenu.js
@@ -5,8 +5,8 @@ import React from 'react'
 
 const ResourcesMenu = ( { resources } ) => (
 	<div>
-		<h1></h1>
-		{ resources.map( resource => <div>{ resource }</div> ) }
+		<h1>Resources List</h1>
+		{ resources.map( resource => <div key={ resource }>{ resource }</div> ) }
 	</div>
 )
 
